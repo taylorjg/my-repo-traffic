41c6dc0361eff8d0d117fec4448881c93ec09c84
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-03-30 00:15:59 +0100

    Tweak to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,6 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
                 test: /\.vue$/,
                 loader: 'vue-loader',
                 options: {
